a9ae8f6459a3716176cbc5b5ffc5a047ab4a5e96
PedroG0612 <pedrogarbatt@gmail.com>
2025-11-12 16:36:39 -0300

    Alterações Js

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+const inputNome = document.getElementById("nome")
+const inputLicao = document.getElementById("licao")
+const inputNota = document.getElementById("nota")
+const resultado = document.getElementById("resultados")
 const notas = document.getElementById("notas")
-const botão = document.getElementById("button")
+const botao = document.getElementById("botao")
 
-botão.addEventListener("click", () => {})
+
+
+botao.addEventListener("click", () => {
+    const aluno = document.createElement("p")
+    aluno.textContent = inputNome.value
+    const atividade = document.createElement("p")
+    atividade.textContent = inputLicao.value
+    const nota = document.createElement("p")
+    nota.textContent = inputNota.value
+    resultado.appendChild(aluno)
+    resultado.appendChild(atividade)
+    resultado.appendChild(nota)
+})
+
+// const nota = inputNota.value.map(p => Number(p.trim()));
+//     soma = nota.reduce((nota, acumulo) => nota + acumulo, 0)
+//     const media = document.createElement("p")
+//     media = soma / nota.length
+//     resultado.appendChild(media)

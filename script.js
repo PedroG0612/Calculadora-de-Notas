cfddd8b74a9c4bdbd5d2ccea05813cb11b155a98
PedroG0612 <pedrogarbatt@gmail.com>
2025-11-13 23:23:33 -0300

    Correções de detalhes e organizando tudo

--- script.js
+++ script.js
@@ -2,27 +2,31 @@ const inputNome = document.getElementById("nome")
 const inputLicao = document.getElementById("licao")
 const inputNota = document.getElementById("nota")
 const resultado = document.getElementById("resultados")
+const mediaArea = document.getElementById("media")
 const notas = document.getElementById("notas")
-const botao = document.getElementById("botao")
+const botaoAdicionar = document.getElementById("botaoAdicionar")
+const botaoMedia = document.getElementById("botaoMedia")
+const notasArray = []
 
-
-
-botao.addEventListener("click", () => {
+botaoAdicionar.addEventListener("click", () => {
     const aluno = document.createElement("p")
     aluno.textContent = inputNome.value
     const atividade = document.createElement("p")
     atividade.textContent = inputLicao.value
     const nota = document.createElement("p")
-    nota.textContent = inputNota.value
+    nota.textContent = Number(inputNota.value)
     resultado.appendChild(aluno)
     resultado.appendChild(atividade)
     resultado.appendChild(nota)
+    notasArray.push(Number(inputNota.value))
+    console.log(notasArray)
 })
 
-const a = a
+botaoMedia.addEventListener("click", () => {
+    soma = notasArray.reduce((nota, acumulo) => nota + acumulo, 0)
+    const media = document.createElement("p")
+    media.textContent = soma / notasArray.length
+    mediaArea.appendChild(media)
+})
 
-// const nota = inputNota.value.map(p => Number(p.trim()));
-//     soma = nota.reduce((nota, acumulo) => nota + acumulo, 0)
-//     const media = document.createElement("p")
-//     media = soma / nota.length
-//     resultado.appendChild(media)
+
